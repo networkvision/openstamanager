e2daf3b09e0219749a001e5e15505f26ad4b7d09
Thomas Zilio <Dasc3er@users.noreply.github.com>
2017-08-04 17:23:25 +0200

    Website generation with Couscous

--- modules/tipi_intervento/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once __DIR__.'/../../core.php';
-
-if (isset($id_record)) {
-    $records = $dbo->fetchArray('SELECT * FROM in_tipiintervento WHERE idtipointervento='.prepare($id_record));
-}
--- plugins/sedi/init.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once __DIR__.'/../../core.php';
-
-$records = $dbo->fetchArray('SELECT * FROM an_sedi WHERE an_sedi.id='.prepare($id_record).' ORDER BY an_sedi.id DESC');
